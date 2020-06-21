98926f8d9364103bf6da6bf039d4ccff30649588
isaakbormental <nikita.dayanoff@gmail.com>
2020-06-21 21:57:06 +0300

    Use postgres instead of sqlite

--- app/settings.py
+++ app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'asana_admin'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'devpostgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'asana_admin.db',
+        'PORT': 5432
     }
 }
 
