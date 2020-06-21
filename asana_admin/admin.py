f2adb045d2a420e9a2b52375c42230a4a8976354
isaakbormental <nikita.dayanoff@gmail.com>
2020-06-21 20:39:18 +0300

    Add models

--- asana_admin/admin.py
+++ asana_admin/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Task, Project, User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    readonly_fields = ["identifier"]
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    readonly_fields = ["identifier"]
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    readonly_fields = ["identifier"]
--- asana_admin/models.py
+++ asana_admin/models.py
@@ -1,3 +1,65 @@
 from django.db import models
 
-# Create your models here.
+TOKEN = ''
+WORKSPACE = ''
+client = asana.Client.access_token(TOKEN)
+
+
+class User(models.Model):
+    identifier = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            result =  client.workspaces.add_user_for_workspace(WORKSPACE, {
+                'user': self.name
+                }, opt_pretty=True)
+            self.identifier = result.get('gid', None)
+
+        super(User, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+class Project(models.Model):
+    identifier = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+
+    def save(self, *args, **kwargs):
+        workspaces = client.workspaces.find_all()
+        if not self.pk:
+            result = client.projects.create_project({
+                'name': self.name, 'workspace': WORKSPACE
+                }, opt_pretty=True)
+            self.identifier = result.get('gid', None)
+        else:
+            result = client.projects.update_project(
+                self.identifier, {'name': self.name}, opt_pretty=True
+            )       
+        
+        super(Project, self).save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    identifier = models.CharField(max_length=200)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    task_text = models.CharField(max_length=1024)
+    implementer = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        data = {
+            'name': self.task_text,
+            'assignee': self.implementer.identifier,
+        }
+
+        if not self.pk:
+            data['projects'] = self.project.identifier,
+            result = client.tasks.create_in_workspace(WORKSPACE, data)
+            self.identifier = result.get('gid', None)
+        else:
+            result = client.tasks.update_task(self.identifier, data, opt_pretty=True)
+
+        super(Task, self).save(*args, **kwargs)
